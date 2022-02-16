b7ff95ae1b973175e886682026a009be7116dc85
memoryleak07 <73183345+memoryleak07@users.noreply.github.com>
2022-02-16 17:54:48 +0100

    Update pand.py

--- pand.py
+++ pand.py
@@ -1,23 +1,40 @@
 # from numpy import dtype
+
+from dateutil import rrule
+from datetime import datetime, timedelta
 import pandas as pd
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 df = pd.read_json('test.json')
 
-print(df.head(5))
-
-#estraggo "transactionsLog" dal json e popolo il dataframe 
+## estraggo "transactionsLog" dal json e popolo il dataframe 
 df = (df["transactionsLog"].apply(pd.Series))
-print(df.head(5))
 
-#rimuovo le colonne inutili dal dataframe
-df = df.drop(columns=['Token','ResCode','ResDescription','Levels','User'])
-print(df.head(5))
+## rimuovo le colonne inutili dal dataframe
+df = df.drop(columns=['Id','Token','ResCode','ResDescription','Levels','User'])
 
-# divido la colonna "Date" in due colonne separate "Date" e "Time"
+# let's examine the types of the column labels
+# print(all(isinstance(column, str) for column in df.columns))
+
+# # divido la colonna "Date" in due colonne separate "Date" e "Time"
 df['Time'] = pd.to_datetime(df['Date']).dt.time
 df['Date'] = pd.to_datetime(df['Date']).dt.date
-print(df.head(5))
 
-#aggiungo una nuova colonna boolean "IsChangeNotDispensed" per i pagamenti completati e non in errore
-df['IsChangeNotDispensed'] = ((df['NotDispensed'] > 0) & (df['TransactionStatus'] == 'COMPLETED') & (df['TransactionCode'] == 'PAY'))
-print(df.head(5))
+## aggiungo una nuova colonna boolean "IsChangeNotDispensed" per i pagamenti completati e non in errore
+df['IsChangeNotDispensed'] = ((df['NotDispensed'] > 0) & ((df['TransactionCode'] == 'PAY') | (df['TransactionCode'] == 'WITHDRAWAL')))
+
+## Filter data between two dates # Soluzione alternativa: filtered_df = (df[df['Date'].between(datestart, dateend)])
+datestart = pd.Timestamp("2021-12-09")
+dateend = pd.Timestamp("2021-12-09" )
+filtered_date_df = df.loc[(df['Date'] >= datestart) & (df['Date'] <= dateend) & (df['TransactionCode'] == 'PAY')]
+
+
+# count = (filtered_date_df['TransactionCode'].value_counts().to_dict())
+# print(count)
+
+print(filtered_date_df.head())
+print(filtered_date_df.dtypes)
